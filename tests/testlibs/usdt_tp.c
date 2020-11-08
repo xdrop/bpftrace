eb3b8ca5c9075626846bc7ba73fae8aa6e1890ea
Panayiotis Pastos <panayiotis.pastos@oaknorth.ai>
2020-11-08 13:22:35 +0000

    Add tests for usdt library probes
    Adding runtime tests that ensure we can list and probe tracepoints not
    present in the main binary, but rather in linked shared libraries.

---
+++ tests/testlibs/usdt_tp.c
@@ -0,0 +1,19 @@
+#ifdef HAVE_SYSTEMTAP_SYS_SDT_H
+#include <sys/sdt.h>
+#else
+#define DTRACE_PROBE2(a, b, c, d) (void)0
+#endif
+#include "usdt_tp.h"
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+long myclock()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  DTRACE_PROBE2(tracetest, testprobe, tv.tv_sec, "Hello world");
+  DTRACE_PROBE2(tracetest, testprobe2, tv.tv_sec, "Hello world2");
+  DTRACE_PROBE2(tracetest2, testprobe2, tv.tv_sec, "Hello world3");
+  return tv.tv_sec;
+}
